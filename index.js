954262f36f22909a3cbc99d45dd85b103c9eec10
kevin-arock <kevinstandlee2002@gmail.com>
2024-06-15 19:32:09 +0530

    Add files.

---
+++ index.js
@@ -0,0 +1,43 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities = []
+
+fetch(endpoint)
+.then((res)=>{
+    return res.json();
+})
+.then((data)=>{
+    cities.push(...data);
+    console.log(data)
+})
+
+function matchCity(word,cities){
+    return cities.filter((obj)=>{
+        let reg = new RegExp(word,'gi');
+        return obj.city.match(reg) || obj.state.match(reg);
+    })
+}
+
+function display(){
+    const mat = matchCity(this.value,cities);
+    const html = mat.map((obj)=>{
+        let reg = new RegExp(this.value,'gi');
+        const city = obj.city.replace(reg,`<span>${this.value}</span>`);
+        const state = obj.city.replace(reg,`<span>${this.value}</span>`);
+        return `
+        <li>
+            <span class="name">${city}, ${state}</span>
+            <span class="population">${obj.population}</span>
+        </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html
+}
+
+
+
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', display);
+searchInput.addEventListener('keyup', display);
